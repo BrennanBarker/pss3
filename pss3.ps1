3363f246538fea86a0acdd46c59706a08f3ef419
Brennan Barker <19918186+BrennanBarker@users.noreply.github.com>
2024-08-20 03:44:47 +0000

    Add S3DownloadObject

--- pss3.ps1
+++ pss3.ps1
@@ -16,13 +16,23 @@ function HMAC_SHA256 {
     return $hmacsha256.ComputeHash($bytes)
 }
 
+function AWSSigV4 {
+    param ([string]$stringToSign, [string]$secretKey, [string]$date, [string]$region)
+    $kSecretKey = [System.Text.Encoding]::UTF8.GetBytes("AWS4$secretKey")
+    $kDate = HMAC_SHA256 $kSecretKey $date
+    $kRegion = HMAC_SHA256 $kDate $region 
+    $kService = HMAC_SHA256 $kRegion "s3" 
+    $kSigning = HMAC_SHA256 $kService "aws4_request" 
+    return Hex (HMAC_SHA256 $kSigning $stringToSign)
+}
+
 function Get-S3GetObjectPresignedUrl {
     param (
         [string]$bucketName,
         [string]$fileKey,
-        [string]$region,
-        [string]$accessKey,
-        [string]$secretKey,
+        [string]$region = $env:AWS_REGION, 
+        [string]$accessKey = $env:AWS_ACCESS_KEY,
+        [string]$secretKey = $env:AWS_SECRET_ACCESS_KEY,
         [datetime]$datetime = (Get-Date).ToUniversalTime(),
         [Int32]$expiration = 86400
     )
@@ -34,17 +44,46 @@ function Get-S3GetObjectPresignedUrl {
     $canonicalRequestHash = Hex (SHA256Hash $canonicalRequest)
     $credentialScope = "$date/$region/s3/aws4_request"
     $stringToSign = "AWS4-HMAC-SHA256`n$amzdate`n$credentialScope`n$canonicalRequestHash"
-    $kSecretKey = [System.Text.Encoding]::UTF8.GetBytes("AWS4$secretKey")
-    $kDate = HMAC_SHA256 $kSecretKey $date
-    $kRegion = HMAC_SHA256 $kDate $region 
-    $kService = HMAC_SHA256 $kRegion "s3" 
-    $kSigning = HMAC_SHA256 $kService "aws4_request" 
-    $signature = Hex (HMAC_SHA256 $kSigning $stringToSign)
+    $signature = AWSSigV4 $stringToSign $secretKey $date $region
     return "https://$bucketName.s3.amazonaws.com/${fileKey}?$canonicalQueryString&X-Amz-Signature=$signature"
 }
 
-# Test
-$presignedUrl = Get-S3GetObjectPresignedUrl "examplebucket" "test.txt" "us-east-1" "AKIAIOSFODNN7EXAMPLE" "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" (Get-Date "24 May 2013").ToUniversalTime()
+function S3DownloadObject {
+    param (
+        [string]$bucketName,
+        [string]$fileKey,
+        [string]$localPath,
+        [string]$region = $env:AWS_REGION, 
+        [string]$accessKey = $env:AWS_ACCESS_KEY,
+        [string]$secretKey = $env:AWS_SECRET_ACCESS_KEY
+    )
+    $datetime = (Get-Date).ToUniversalTime()
+    $amzdate = $datetime.ToString("yyyyMMddTHHmmssZ")
+    $date = $datetime.ToString("yyyyMMdd")
+    $scope = "$date/$region/s3/aws4_request"
+    $emptyHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    $canonicalHeaders = "host:$bucketName.s3.amazonaws.com`nx-amz-content-sha256:$emptyHash`n`x-amz-date:$amzdate`n"
+    $signedHeaders = "host;x-amz-content-sha256;x-amz-date"
+    $canonicalRequest = "GET`n/$fileKey`n`n$canonicalHeaders`n$signedHeaders`n$emptyHash"
+    $canonicalRequestHash = Hex (SHA256Hash $canonicalRequest)
+    $stringToSign = "AWS4-HMAC-SHA256`n$amzdate`n$scope`n$canonicalRequestHash"
+    $signature = AWSSigV4 $stringToSign $secretKey $date $region
+    
+    Invoke-WebRequest "https://$bucketName.s3.amazonaws.com/$fileKey" -Headers @{
+        "x-amz-date" = $amzdate
+        "Authorization" = "AWS4-HMAC-SHA256 Credential=$accessKey/$scope,SignedHeaders=$signedHeaders,Signature=$signature"
+        "x-amz-content-sha256" = $emptyHash
+    } -OutFile $localPath -SkipHeaderValidation
+}
+
+# Tests
+$testBucket = "examplebucket"
+$testObject = "test.txt"
+$testRegion = "us-east-1"
+$testAccessKey = "AKIAIOSFODNN7EXAMPLE"
+$testSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+$presignedUrl = Get-S3GetObjectPresignedUrl $testBucket $testObject $testRegion $testAccessKey $testSecretKey (Get-Date "24 May 2013").ToUniversalTime()
 $testPresignedUrl = "https://examplebucket.s3.amazonaws.com/test.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIOSFODNN7EXAMPLE%2F20130524%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20130524T000000Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=aeeed9bbccd4d02ee5c0109b86d86835f995330da4c265957d157751f604d404"
 if ($presignedUrl -ne $testPresignedUrl) {
     Write-Output "TEST FAIL: The presigned urls are not equal."
@@ -66,6 +105,5 @@ $fileKeys = @(
 )
 $localFilePath = "/workspaces/pss3" 
 foreach ($fileKey in $fileKeys) {
-    $url = Get-S3GetObjectPresignedUrl $env:S3_BUCKET $fileKey $env:AWS_REGION $env:AWS_ACCESS_KEY $env:AWS_SECRET_ACCESS_KEY -expiration 1
-    Invoke-WebRequest -Uri $url -OutFile $localFilePath -MaximumRedirection 0
+    S3DownloadObject $env:S3_BUCKET $fileKey $localFilePath 
 }
