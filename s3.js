81170ed56cbbfbe3b0a45f6bd353aeb8b443d6a5
Brennan Barker <19918186+BrennanBarker@users.noreply.github.com>
2024-09-18 03:05:08 +0000

    add table

--- s3.js
+++ s3.js
@@ -20,17 +20,78 @@ async function awsSigV4(secretAccessKey, date, region, stringToSign) {
 
 const hex = (ab) => new Uint8Array(ab).reduce((x,y) => x += y.toString(16).padStart(2, '0'), '')
 
+function getSignedHeaders(canonicalHeaders) {
+    return canonicalHeaders.split('\n').filter(x=>x!='').map(x=>x.split(':')[0]).join(';')
+}
+
 async function s3GetObjectPresignedUrl(bucket, key, region, awsAccessKeyId, awsSecretAccessKey, datetime = new Date(), expiration = 86400) {
+    const algorithm = 'AWS4-HMAC-SHA256'
     const amzdate = datetime.toISOString().replaceAll('-', '').replaceAll(':', '').split('.')[0]+'Z'
     const date = amzdate.split('T')[0]
     const credentialScope = `${date}/${region}/s3/aws4_request`
-    const credential = `${awsAccessKeyId}/${credentialScope}`.replaceAll("/", "%2F")
-    const canonicalQueryString = `X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=${credential}&X-Amz-Date=${amzdate}&X-Amz-Expires=${expiration}&X-Amz-SignedHeaders=host`
-    const canonicalRequest = `GET\n/${key}\n${canonicalQueryString}\nhost:${bucket}.s3.amazonaws.com\n\nhost\nUNSIGNED-PAYLOAD`
+    const credential = `${awsAccessKeyId}/${credentialScope}`
+    const payloadHash = 'UNSIGNED-PAYLOAD'
+    const host = `${bucket}.s3.amazonaws.com`
+    const canonicalHeaders = `host:${host}\n`
+    const signedHeaders = getSignedHeaders(canonicalHeaders)
+    const canonicalQueryString = `X-Amz-Algorithm=${algorithm}&X-Amz-Credential=${credential.replaceAll("/", "%2F")}&X-Amz-Date=${amzdate}&X-Amz-Expires=${expiration}&X-Amz-SignedHeaders=${signedHeaders}`
+    const canonicalUri = `/${key}`
+    const canonicalRequest = `GET\n${canonicalUri}\n${canonicalQueryString}\n${canonicalHeaders}\n${signedHeaders}\n${payloadHash}`
     const canonicalRequestHash = hex(await sha256Hash(canonicalRequest))
-    const stringToSign = `AWS4-HMAC-SHA256\n${amzdate}\n${credentialScope}\n${canonicalRequestHash}`
+    const stringToSign = `${algorithm}\n${amzdate}\n${credentialScope}\n${canonicalRequestHash}`
     const signature = await awsSigV4(awsSecretAccessKey, date, region, stringToSign)
     return `https://${bucket}.s3.amazonaws.com/${key}?${canonicalQueryString}&X-Amz-Signature=${signature}`
+}
+
+async function s3ListObjects(bucket, region, awsAccessKeyId, awsSecretAccessKey, datetime = new Date()) {
+    const algorithm = 'AWS4-HMAC-SHA256'
+    const amzdate = datetime.toISOString().replaceAll('-', '').replaceAll(':', '').split('.')[0]+'Z'
+    const date = amzdate.split('T')[0]
+    const credentialScope = `${date}/${region}/s3/aws4_request`
+    const credential = `${awsAccessKeyId}/${credentialScope}`
+    const payloadHash = hex(await sha256Hash(''));
+    const host = `${bucket}.s3.amazonaws.com`
+    const canonicalHeaders = `host:${host}\nx-amz-content-sha256:${payloadHash}\nx-amz-date:${amzdate}\n`;
+    const signedHeaders = getSignedHeaders(canonicalHeaders)
+    const canonicalQueryString = 'list-type=2';
+    const canonicalUri = '/'
+    const canonicalRequest = `GET\n${canonicalUri}\n${canonicalQueryString}\n${canonicalHeaders}\n${signedHeaders}\n${payloadHash}`
+    const canonicalRequestHash = hex(await sha256Hash(canonicalRequest))
+    const stringToSign = `${algorithm}\n${amzdate}\n${credentialScope}\n${canonicalRequestHash}`;
+    const signature = await awsSigV4(awsSecretAccessKey, date, region, stringToSign)
+
+    const authorizationHeader = `${algorithm} Credential=${credential}, SignedHeaders=${signedHeaders}, Signature=${signature}`;
+
+    const response = await fetch(`https://${host}?${canonicalQueryString}`, {
+        method: 'GET',
+        headers: {
+            'x-amz-date': amzdate, 
+            'x-amz-content-sha256': payloadHash,
+            'Authorization': authorizationHeader
+        }
+    });
+    const responseText = await response.text();
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(responseText, "application/xml");
+  
+    const tagContent = (doc, tag) => Array.from(xmlDoc.getElementsByTagName(tag)).map(el => el.textContent)
+    const keys = tagContent(xmlDoc, 'Key');
+    const sizes = tagContent(xmlDoc, 'Size');
+    const last_modifieds = tagContent(xmlDoc, 'LastModified')
+  
+    return keys.map((key, i) => ({
+        key: key,
+        size: sizes[i],
+        last_modified: last_modifieds[i]
+    }));
+}
+
+function formatBytes(bytes) {
+    if (bytes === 0) return '0 Bytes';
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    const formattedBytes = parseFloat((bytes / Math.pow(1024, i)).toFixed(2));
+    return `${formattedBytes} ${sizes[i]}`;
 }
 
 document.head.innerHTML = ''
@@ -41,15 +102,29 @@ document.querySelector('#config-file').addEventListener('change', function(event
         const fileContent = e.target.result;
         const config = Object.fromEntries(fileContent.split('\n').map(line => line.split('=')).filter(([x,y])=>x.length > 0))
         const bucket = window.location.host.replace('.s3.amazonaws.com','')
-        document.body.innerHTML = ''
-        for (let key of config.keys.split(',')) {
-            key = key.trim()
+
+        const contents = await s3ListObjects(bucket, config.AWS_REGION, config.AWS_ACCESS_KEY, config.AWS_SECRET_ACCESS_KEY)
+
+        const tbody = document.createElement('tbody')
+        for (let item of contents) {
+            const key = item.key.trim()
             const href = await s3GetObjectPresignedUrl(bucket, key, config.AWS_REGION, config.AWS_ACCESS_KEY, config.AWS_SECRET_ACCESS_KEY)
             const link = document.createElement('a')
             link.setAttribute('href', href)
             link.textContent = key
-            document.body.innerHTML += link.outerHTML+'<br />'
+            const row = document.createElement('tr')
+            const td_key = document.createElement('td')
+            td_key.appendChild(link)
+            row.appendChild(td_key)
+            const td_size = document.createElement('td')
+            td_size.textContent = formatBytes(item.size)
+            row.appendChild(td_size)
+            const td_last_modified = document.createElement('td')
+            td_last_modified.textContent = item.last_modified
+            row.appendChild(td_last_modified)
+            tbody.appendChild(row)
         }
+        document.body.innerHTML = `<table><thead><tr><th>Key</th><th>Size</th><th>Last Modified</th></tr></thead><tbody>${tbody.innerHTML}</tbody></table>`
     };
     reader.readAsText(event.target.files[0]);
 });
